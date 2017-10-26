49810a8fe370bccb09d1ff28faf29cf8ab15232a
Bob Blau <rblau@nab.org>
2017-10-26 15:09:39 -0400

    Initial Commit

---
+++ KEYCODES.js
@@ -0,0 +1,31 @@
+var keycodes = {
+    LEFT: 37,       // left arrow
+    RIGHT: 39,      // right arrow
+    UP: 38,         // up arrow
+    DOWN: 40,       // down arrow
+    SELECT: 13,     // enter
+    EXIT: 27,       // esc
+    MENU: 77,       // 'm'
+    GUIDE: 69,      // 'e'
+    INFO: 73,       // 'i'
+    RED: 82,        // 'r'
+    GREEN: 71,      // 'g'
+    YELLOW: 89,     // 'y'
+    BLUE: 66,       // 'b'
+    PLAY: 80,       // 'p'
+    PAUSE: 81,      // 'q'
+    REW: 109,       // Subtract
+    FFW: 107,       // Add
+    SLOW: 68,       // 'd'
+    FW: 70,         // 'f'
+    KEY0: 96,   //numpad 0
+    KEY1: 97,   //numpad 1
+    KEY2: 98,   //numpad 2
+    KEY3: 99,   //numpad 3
+    KEY4: 100,  //numpad 4
+    KEY5: 101,  //numpad 5
+    KEY6: 102,  //numpad 6
+    KEY7: 103,  //numpad 7
+    KEY8: 104,  //numpad 8
+    KEY9: 105   //numpad 9
+}
